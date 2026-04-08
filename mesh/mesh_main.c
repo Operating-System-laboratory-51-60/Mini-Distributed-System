c6a95abac2096e1ecffd74c24fa88f5587510ae8
UllasDas004 <ullasdas110904@gmail.com>
2026-04-08 18:24:17 +0530

    multi machine bug fixed

--- mesh/mesh_main.c
+++ mesh/mesh_main.c
@@ -103,7 +103,7 @@ int main(int argc, char *argv[]) {
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(my_port);
-    inet_pton(AF_INET, my_ip, &server_addr.sin_addr);
+    server_addr.sin_addr.s_addr = INADDR_ANY; // Bind to all interfaces to avoid EADDRNOTAVAIL
 
     if (bind(listen_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
         log_error("MESH_MAIN", "Failed to bind socket", strerror(errno));
